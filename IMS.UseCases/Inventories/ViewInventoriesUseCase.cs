61c1d418023fa82b9edee55516b6b5f89f08676a
juanmlinor <jumalire55@outlook.com>
2024-09-10 13:35:34 -0600

    first commit

---
+++ IMS.UseCases/Inventories/ViewInventoriesUseCase.cs
@@ -0,0 +1,12 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases.inventories
+{
+    public class ViewInventoriesUseCase
+    {
+        //public IEnumerable<Inventory> ExecuteAsync(string name = "")
+        //{
+
+        //}
+    }
+}
---
+++ IMS.UseCases/Inventories/ViewInventoryByIdUseCase.cs
@@ -0,0 +1,16 @@
+﻿namespace IMS.UseCases
+{
+    public class ViewInventoryByIdUseCase
+    //: IViewInventoryByIdUseCase
+    {
+        //private readonly IInventoryRepository inventoryRepository;
+        //public ViewInventoryByIdUseCase(IInventoryRepository inventoryRepository)
+        //{
+        //    this.inventoryRepository = inventoryRepository;
+        //}
+        //public async Task<Inventory?> ExecuteAsync(int inventoryId)
+        //{
+        //    return await this.inventoryRepository.GetInventoryByIdAsync(inventoryId);
+        //}
+    }
+}
